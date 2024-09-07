1a16083f9dd16776f0e6bada4e26f00e9ec2b579
KeitaTsukuba <keitainterncoins@gmail.com>
2024-09-07 13:40:40 +0900

    add stripe code

---
+++ go.mod
@@ -0,0 +1,10 @@
+module stripe-webhook-test
+
+go 1.19
+
+require github.com/stripe/stripe-go v70.15.0+incompatible
+
+require (
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/stripe/stripe-go"
+)
+
+func main() {
+	http.HandleFunc("/webhook", func(w http.ResponseWriter, req *http.Request) {
+		const MaxBodyBytes = int64(65536)
+		req.Body = http.MaxBytesReader(w, req.Body, MaxBodyBytes)
+		payload, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading request body: %v\n", err)
+			w.WriteHeader(http.StatusServiceUnavailable)
+			return
+		}
+
+		event := stripe.Event{}
+
+		if err := json.Unmarshal(payload, &event); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to parse webhook body json: %v\n", err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		// Unmarshal the event data into an appropriate struct depending on its Type
+		switch event.Type {
+		case "payment_intent.succeeded":
+			var paymentIntent stripe.PaymentIntent
+			err := json.Unmarshal(event.Data.Raw, &paymentIntent)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error parsing webhook JSON: %v\n", err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			handlePaymentIntentSucceeded(paymentIntent)
+		case "payment_method.attached":
+			var paymentMethod stripe.PaymentMethod
+			err := json.Unmarshal(event.Data.Raw, &paymentMethod)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error parsing webhook JSON: %v\n", err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			handlePaymentMethodAttached(paymentMethod)
+
+		default:
+			fmt.Fprintf(os.Stderr, "Unhandled event type: %s\n", event.Type)
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	fmt.Println("Starting server at :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
+
+func handlePaymentIntentSucceeded(paymentIntent stripe.PaymentIntent) {
+	fmt.Printf("PaymentIntent was successful: %v\n", paymentIntent.ID)
+}
+
+func handlePaymentMethodAttached(paymentMethod stripe.PaymentMethod) {
+	fmt.Printf("PaymentMethod was attached: %v\n", paymentMethod.ID)
+}
